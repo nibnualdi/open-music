8fbdf2bfb92c9bd4edd99b05d331fe5322d28806
nibnualdi <nibnualdi@gmail.com>
2025-02-20 19:23:55 +0700

    add exceptions and register the service on server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/AlbumsService');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +16,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
